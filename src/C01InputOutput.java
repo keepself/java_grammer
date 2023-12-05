a8a26fc0f7e4286fb1d2dcac52f5eb8d0377cda1
keepself <keepself@naver.com>
2023-12-05 11:13:57 +0900

    12/05 java

---
+++ src/C01InputOutput.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+import java.util.ServiceConfigurationError;
+
+public class C01InputOutput {
+    public static void main(String[] args) {
+
+////        출력
+//        int a = 20;
+////        print 는 출력후 줄바꿈 없음. println은 출력후 줄바꿈
+//        System.out.print(a);
+//        String myString = "hello world";
+////        문자열과 숫자를 합하면 문자가 된다-5
+//        System.out.println(myString + a);    }
+//          숫자와 숫자를 더하면 더하기 () -> 연산된다
+                System.out.println(10+20);
+
+        //        입력 : System.in(키보드입력) + Scanner(입력을위한클래스)
+        Scanner myScan = new Scanner(System.in);
+//        System.out.println("아무 문자열을 입력해주세요");
+////          nextLine은 입력 받은 데이터를 한 줄 읽어서, String으로 리턴
+//        String inputs = myScan.nextLine();
+//        System.out.println("사용자가 입력한 문자열 : " + inputs);
+//
+////          nextLine은 입력 받은 데이터를 한 줄 읽어서, int으로 리턴
+//        int inputs_int = myScan.nextInt();
+//        System.out.println("사용자가 입력한 숫자는 : " + inputs_int);
+
+//        System.out.println("더하기할 두 숫자를 입력해주세요");
+//        int a1 = myScan.nextInt();
+//        int a2 = myScan.nextInt();
+//        int sum = a1+a2;
+//        System.out.println("두 수를 더한 값은 "+(a1+a2)+"입니다. ");
+
+        System.out.println("실수를 입력해주세요");
+        double input_double = myScan.nextDouble();
+        System.out.println("입력하실 실수는 : " + input_double);
+
+        System.out.println("true 또는 false를 입력해주세요");
+        boolean input_boolean = myScan.nextBoolean();
+        System.out.println("입력하신 참/거짓은 : "+input_boolean);
+
+//        입출력 시스템 메모리 해제
+        myScan.close();
+
+
+    }
+}
---
+++ src/C02Variable.java
@@ -0,0 +1,122 @@
+import java.math.BigDecimal;
+import java.sql.SQLOutput;
+
+public class C02Variable {
+        public static void main(String[] args) {
+//               byte로 썻을때 -128~127까지의 표현이 가능하고, 그 이상을 세팅할때 오버/언더 플로우 발생.
+                byte num1 = 127;
+                byte num2 = -128;
+                num1++;
+                num2--;
+                System.out.println(num1);
+                System.out.println(num2);
+
+
+//      실수 : float, double(기본)
+                float f1 = 1.123456789f;
+                double b1 = 1.123456789;
+                System.out.println("f1 : " + f1);
+                System.out.println("b1 : " + b1);
+//               부동소수점 오차 테스트
+//                double double_uum = 0.1;
+//                미세오차는 조정되어 정상적으로 출력
+//                        System.out.println(double_uum);
+//               반복적인 연산시 오차가 확대되어 오차확인가능
+//                for(int i=0;i<100;i++){
+//                        System.out.println(i);
+//                }
+//                double double_uum = 0.1;
+//                for(int i=0; i<1000; i++){
+//                        System.out.println(i);
+////              반복적인 연산시 오차가 확대되어 오차 확인가능
+//                double total = 0;
+//                for (int i = 0; i < 100; i++) {
+//                        total = total + 0.1 * 10;
+//                }
+//                System.out.println(total/10);
+//
+//                double d1 = 1.03;
+//                double d2 = 0.42;
+//                System.out.println(d1-d2);
+//////
+////                저장할때는 문자열 -> 연산할때는 정수로 변환 -> 최종결과는 실수로 반환
+//                BigDecimal myBig = new BigDecimal("1.03");
+//                BigDecimal myBig2 = new BigDecimal("0.42");
+
+//                문자 : char
+
+//                        char my_char = '가';
+//                        System.out.println(my_char);
+//
+////                      boolean : true(1) or false(0)
+//                boolean my_bool = true;
+//                System.out.println(my_bool);
+//                if(my_bool==true){
+//                        System.out.println("조건식이 참입니다.");
+//
+//                        int bool_num1 = 20;
+//                        int bool_num2 = 10;
+//                        if(bool_num1 > bool_num2){
+//                                System.out.println("조건식이 참입니다");
+//                        }
+
+//                      묵시적 타입변환
+                char ch1 = 'a';
+                int ch1_num = ch1;
+                System.out.println(ch1_num);
+
+
+                int my_int1 = 10;
+                double my_double1 = my_int1;
+                System.out.println(my_double1);
+
+//
+//                int my_int1 = 10;
+//                double my_double1 = my_int1;
+//                System.out.println(my_double1);
+// 에러 발생 : my_int1 = my_double1;
+//                double -> int 명시적 타입은 가능 : 소수점값 손실발생 가능성이 있다.
+//                my_int1 = (int)my_double1;
+//        double my_double2 = 7.2f;
+//                System.out.println(my_double2);
+//
+//
+//
+////      명시적 타입변환
+//                char my_char2 ='b';
+//                int char_num =(int)my_char2;
+//       int a 가 1 int b가 4일때 둘을 나눈값을 int에 답아 출력 . double에 담아 출력
+
+                int a = 1;
+                int b = 4;
+                int c = a / b;
+                double d = a / b;
+
+                System.out.println(c);
+                System.out.println(d);
+                double d2 = (double)a/(double)b;
+                System.out.println(d2);
+
+//              변수와 상수
+//              선언과 동시에 초기화
+                int a1 = 10;
+//              변수값 변경
+                a1 = 20;
+//              선언만 한 뒤에 나중에 초기화
+                int a2; //선언만 됐을때는 값이 0으로 초기화
+//                System.out.println(a2);
+                a2 = 20;
+//                상수는 값의 재할당이 불가능
+                final int AGES = 20 ;
+//                상수는 값의 변경이 불가능AGES = 30;  (파이널붙음)
+//                상수는 선언만 한 뒤에 나중에 초기화 하는 방식이 java8이전에는 안됏엇지만, 이후 가능해짐
+                final int AGES2;
+                AGES2 = 20;
+
+
+
+
+
+
+        }
+}
